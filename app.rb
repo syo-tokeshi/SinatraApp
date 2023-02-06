5952280984bba6668af7860d1ec62f9dce72bc99
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-06 18:08:10 +0900

    簡易版ルーティング + コントローラーの作成

--- app.rb
+++ app.rb
@@ -2,5 +2,33 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  'wa'
+  redirect '/memos'
+end
+
+get '/memos' do
+  erb :index
+end
+
+get '/memos/new' do
+  erb :new
+end
+
+post '/memos' do
+  redirect '/memos'
+end
+
+get '/memos/:id' do
+  erb :show
+end
+
+patch '/memos/:id' do
+  redirect "/memos/#{params[:id]}"
+end
+
+delete '/memos/:id' do
+  redirect '/memos'
+end
+
+get '/memos/:id/edit' do
+  erb :edit
 end
