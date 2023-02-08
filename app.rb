63ceca691ee77ab892ee0dce42338c9285d604f2
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-08 12:07:20 +0900

    showアクションを呼び出し、個別のメモを表示する + 関数のリファクタリング

--- app.rb
+++ app.rb
@@ -2,6 +2,22 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'debug'
 
+def memos_divided_per_column
+  memos = File.open("asset/memos.txt", "r") do |f|
+    f.read.split("\n")
+  end
+  memos.map{ |a|
+    a.split(",")
+  }
+end
+
+def search_for_memos_by_id(params_id)
+  memos = memos_divided_per_column
+  memos.each do |memo|
+    return memo if memo[0].to_i == params_id
+  end
+end
+
 def remake_memos_add_number(memos_with_added_id)
   File.open("asset/memos.txt","w") do |text|
     memos_with_added_id.each { |memo|
@@ -11,15 +27,10 @@ def remake_memos_add_number(memos_with_added_id)
 end
 
 def add_id_to_memos
-  memos = File.open("asset/memos.txt", "r") do |f|
-    f.read.split("\n")
-  end
-  @memos_divided_per_column = memos.map{ |a|
-    a.split(",")
-  }
-  @memos_divided_per_column[-1].unshift(@memos_divided_per_column[-2][0].to_i + 1) if @memos_divided_per_column.count > 1
-  @memos_divided_per_column[-1].unshift(1) if @memos_divided_per_column.count == 1
-  remake_memos_add_number(@memos_divided_per_column)
+  memos = memos_divided_per_column
+  memos[-1].unshift(memos[-2][0].to_i + 1) if memos.count > 1
+  memos[-1].unshift(1) if memos.count == 1
+  remake_memos_add_number(memos)
 end
 
 def write_memos(title,content)
@@ -55,6 +66,8 @@ post '/memos' do
 end
 
 get '/memos/:id' do
+  params_id = params[:id].to_i
+  @memo = search_for_memos_by_id(params_id)
   erb :show
 end
 
