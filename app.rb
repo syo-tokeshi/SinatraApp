561cc39701e86fa271b17dafa8b3ee66fc21e399
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-08 11:36:57 +0900

    showアクションに個別のメモをidを指定して渡す処理を追加

--- app.rb
+++ app.rb
@@ -2,6 +2,26 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'debug'
 
+def remake_memos_add_number(memos_with_added_id)
+  File.open("asset/memos.txt","w") do |text|
+    memos_with_added_id.each { |memo|
+      text.puts("#{memo[0]},#{memo[1]},#{memo[2]}")
+    }
+  end
+end
+
+def add_id_to_memos
+  memos = File.open("asset/memos.txt", "r") do |f|
+    f.read.split("\n")
+  end
+  @memos_divided_per_column = memos.map{ |a|
+    a.split(",")
+  }
+  @memos_divided_per_column[-1].unshift(@memos_divided_per_column[-2][0].to_i + 1) if @memos_divided_per_column.count > 1
+  @memos_divided_per_column[-1].unshift(1) if @memos_divided_per_column.count == 1
+  remake_memos_add_number(@memos_divided_per_column)
+end
+
 def write_memos(title,content)
   File.open("asset/memos.txt","a") do |text|
     text.puts("#{title},#{content}")
@@ -29,9 +49,8 @@ end
 post '/memos' do
   @title = params[:title]
   @content = params[:content]
-  logger.info @title
-  logger.info @content
   write_memos(@title,@content)
+  add_id_to_memos
   redirect '/memos'
 end
 
